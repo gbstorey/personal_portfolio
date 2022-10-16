1e1370c8bd65238a92db1625028442bd18ffa02e
gbstorey <gbstorey@asu.edu>
2022-10-16 13:38:00 -0700

    added mobile breakpoints to avoid weird image distortion

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -26,10 +26,10 @@ export default function Projects() {
                   <div className="flex relative h-full">
                     <img
                       alt="gallery"
-                      className="absolute inset-0 h-full w-full object-cover object-top"
+                      className="absolute inset-0 h-full w-full object-cover object-left-top opacity-0 lg:opacity-100"
                       src={project.image}
                     /> 
-                    <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-800 bg-gray-900 opacity-0 hover:opacity-100">
+                    <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-800 bg-gray-900 opacity-100 lg:opacity-0 hover:opacity-100">
                       <h2 className="tracking-widest text-sm title-font font-medium text-green-400 mb-1">
                         {project.subtitle}
                       </h2>
