5252dc8901a6bd0abdba227ba1419a5faec627b4
gbstorey <gbstorey@asu.edu>
2022-10-16 16:43:06 -0700

    added hidden form input

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -13,6 +13,7 @@ export default function Contact() {
 
     function handleSubmit(e) {
         e.preventDefault();
+
         fetch("/", {
             method: "POST",
             headers: {"Content-Type": "application/x-www-form-urlencoded"},
@@ -21,9 +22,6 @@ export default function Contact() {
         .then(() => alert("Message sent!"))
         .catch((error) => alert(error));
     }
-
-
-
 
 
     return (
@@ -70,12 +68,12 @@ export default function Contact() {
               name="contact"
               onSubmit={handleSubmit}
               className="lg:w-1/3 md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
+              <input type="hidden" name="form-name" value="contact" />
               <h2 className="text-white sm:text-4xl text-3xl mb-1 font-medium title-font">
-                Hire Me
+                Contact Me
               </h2>
               <p className="leading-relaxed mb-5">
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illum
-                suscipit officia aspernatur veritatis. Asperiores, aliquid?
+                Please reach out using this form. I would love to hear from you!
               </p>
               <div className="relative mb-4">
                 <label htmlFor="name" className="leading-7 text-sm text-gray-400">
